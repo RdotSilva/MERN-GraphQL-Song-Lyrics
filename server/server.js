70ecbd7d898d5eae41e5f6744375158aa503cfa5
Ryan Silva <RyandotSilva@gmail.com>
2019-08-09 17:05:30 -0400

    Add graphQL server

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+// const models = require("./models");
+const expressGraphQL = require("express-graphql");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+// const schema = require("./schema/schema");
+const keys = require("../keys/devKeys");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
+
+app.use(
+	"/graphql",
+	expressGraphQL({
+		schema,
+		graphiql: true
+	})
+);
+
+module.exports = app;
