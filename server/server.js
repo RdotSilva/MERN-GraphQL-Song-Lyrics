48b10841b48d998e8bc449fdb27eb3c4315410a8
Ryan Silva <RyandotSilva@gmail.com>
2019-08-09 17:48:32 -0400

    Update mongoose with useNewUrlParser

--- server/server.js
+++ server/server.js
@@ -7,15 +7,18 @@ const schema = require("./schema/schema");
 const keys = require("../keys/devKeys");
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 mongoose
-	.connect(keys.mongoURI)
+	.connect(keys.mongoURI, { useNewUrlParser: true })
 	.then(() => console.log("MongoDB Connected"))
 	.catch(err => console.log(err));
 
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+	res.send("Server is up!");
+});
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
 
