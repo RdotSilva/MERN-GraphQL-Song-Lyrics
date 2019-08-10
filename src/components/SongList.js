a9f54567e12b5a04ea7543a9876d6d6fdf269f25
Ryan Silva <RyandotSilva@gmail.com>
2019-08-10 11:35:33 -0400

    Add song list query

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
 const SongList = () => {
+	const { loading, error, data } = useQuery(query);
+	console.log(data);
 	return <div>Song List</div>;
 };
+
+const query = gql`
+	{
+		songs {
+			title
+		}
+	}
+`;
 
 export default SongList;
