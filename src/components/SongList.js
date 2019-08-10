a943b9f66fdc378c35cb1ccbb0b796a00f8b2599
Ryan Silva <RyandotSilva@gmail.com>
2019-08-10 11:45:00 -0400

    Add renderSongs function

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -4,8 +4,17 @@ import { useQuery } from "@apollo/react-hooks";
 
 const SongList = () => {
 	const { loading, error, data } = useQuery(query);
-	console.log(data);
-	return <div>Song List</div>;
+
+	const renderSongs = () => {
+		return data.songs.map(song => {
+			return <li>{song.title}</li>;
+		});
+	};
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error :(</p>;
+
+	return <div>{renderSongs()}</div>;
 };
 
 const query = gql`
