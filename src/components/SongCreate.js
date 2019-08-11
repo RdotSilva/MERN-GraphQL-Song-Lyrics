e98b5ef5d8517c55a5be947a04417d2e07b54fc7
Ryan Silva <RyandotSilva@gmail.com>
2019-08-11 18:27:26 -0400

    Add controlled input for song title

--- src/components/SongCreate.js
+++ src/components/SongCreate.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SongCreate = () => {
+	const [songData, setSongData] = useState({
+		title: ""
+	});
+
+	const { title } = songData;
+
+	const onChange = e => {
+		setSongData({ ...songData, [e.target.name]: e.target.value });
+	};
+
 	return (
 		<div>
 			<h1>Create a new song!</h1>
+			<form>
+				<label>Song Title:</label>
+				<input name="title" onChange={e => onChange(e)} value={title} />
+			</form>
 		</div>
 	);
 };
