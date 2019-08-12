52519576f158af38aa5760fa794d8d0bbb61e30b
Ryan Silva <RyandotSilva@gmail.com>
2019-08-12 16:07:44 -0400

    Update onSubmit to go back after submission

--- src/components/SongCreate.js
+++ src/components/SongCreate.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 
+import { Link } from "react-router-dom";
+
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/react-hooks";
 
-const SongCreate = () => {
+const SongCreate = props => {
 	const [songData, setSongData] = useState({
 		title: ""
 	});
 
-	const [addSong, { data }] = useMutation(ADD_SONG);
+	const [addSong, { data, called }] = useMutation(ADD_SONG);
 
 	const { title } = songData;
 
@@ -19,10 +21,14 @@ const SongCreate = () => {
 	const onSubmit = e => {
 		e.preventDefault();
 		addSong({ variables: { title: title } });
+		props.history.goBack();
 	};
 
 	return (
 		<div>
+			<Link to="/songlist" className="btn-floating btn-small blue">
+				Back
+			</Link>
 			<h1>Create a new song!</h1>
 			<form onSubmit={onSubmit}>
 				<label>Song Title:</label>
