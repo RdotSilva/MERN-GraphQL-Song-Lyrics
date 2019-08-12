82eef19bb46e1e190a55297074626f504913a44c
Ryan Silva <RyandotSilva@gmail.com>
2019-08-12 15:26:02 -0400

    Add onSubmit to form

--- src/components/SongCreate.js
+++ src/components/SongCreate.js
@@ -11,10 +11,14 @@ const SongCreate = () => {
 		setSongData({ ...songData, [e.target.name]: e.target.value });
 	};
 
+	const onSubmit = e => {
+		e.preventDefault();
+	};
+
 	return (
 		<div>
 			<h1>Create a new song!</h1>
-			<form>
+			<form onSubmit={onSubmit()}>
 				<label>Song Title:</label>
 				<input name="title" onChange={e => onChange(e)} value={title} />
 			</form>
