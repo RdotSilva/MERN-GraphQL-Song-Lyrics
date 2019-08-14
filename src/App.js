d80e5665715e623ba0301327f78873479d3701cf
Ryan Silva <RyandotSilva@gmail.com>
2019-08-14 19:11:14 -0400

    Import styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./style/style.css";
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import ApolloClient from "apollo-boost";
