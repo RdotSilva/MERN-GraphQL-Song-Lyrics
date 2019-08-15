a03bd7c479fce6a86c25bb10d8566379004c904b
Ryan Silva <RyandotSilva@gmail.com>
2019-08-15 15:25:13 -0400

    Add LyricList component

---
+++ src/components/LyricList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { useMutation } from "@apollo/react-hooks";
+
+const LyricList = () => {
+	return (
+		<div>
+			<ul>Lyric List</ul>
+		</div>
+	);
+};
+
+export default LyricList;
