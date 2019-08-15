3d427fabd110a2ff05c5db47f0961dbf00cf8d17
Ryan Silva <RyandotSilva@gmail.com>
2019-08-14 20:17:56 -0400

    Add useQuery hook for fetch song

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
 
-const SongDetail = () => {
+import FETCH_SONG from "../queries/fetchSong";
+
+const SongDetail = props => {
+	const { loading, data } = useQuery(FETCH_SONG, {
+		variables: { id: props.match.params.id }
+	});
+
+	if (loading) return <h1>Loading</h1>;
+
+	console.log(data);
+
 	return (
 		<div>
 			<h3>Song Detail</h3>
