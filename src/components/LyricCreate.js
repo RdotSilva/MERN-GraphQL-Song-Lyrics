474d3a89431ed850dd83b1d5a6e282457c770d9d
Ryan Silva <RyandotSilva@gmail.com>
2019-08-15 14:13:15 -0400

    Add LyricCreate component

---
+++ src/components/LyricCreate.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LyricCreate = () => {
+	return (
+		<div>
+			<form>
+				<label>Add a Lyric</label>
+				<input />
+			</form>
+		</div>
+	);
+};
+
+export default LyricCreate;
