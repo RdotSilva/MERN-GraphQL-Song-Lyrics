2842ad54d3ba148a60539ed8d5539aeb4f1902d9
Ryan Silva <RyandotSilva@gmail.com>
2019-08-15 14:27:28 -0400

    Add handleChangeInput function

--- src/components/LyricCreate.js
+++ src/components/LyricCreate.js
@@ -3,11 +3,18 @@ import React, { useState } from "react";
 const LyricCreate = () => {
 	const [content, setContent] = useState("");
 
+	const handleInputChange = content => {
+		setContent(content);
+	};
+
 	return (
 		<div>
 			<form>
 				<label>Add a Lyric</label>
-				<input />
+				<input
+					value={content}
+					onChange={e => handleInputChange(e.target.value)}
+				/>
 			</form>
 		</div>
 	);
