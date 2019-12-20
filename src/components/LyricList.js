51add1ccd05f565e7fcc6ac6b0b1bbb19fab6d79
RdotSilva <RyandotSilva@gmail.com>
2019-12-20 18:39:14 -0500

    Update like button style

--- src/components/LyricList.js
+++ src/components/LyricList.js
@@ -5,47 +5,52 @@ import { useMutation } from "@apollo/react-hooks";
 import LIKE_LYRIC from "../queries/likeLyric";
 
 const LyricList = props => {
-	const [likeLyric] = useMutation(LIKE_LYRIC);
+  const [likeLyric] = useMutation(LIKE_LYRIC);
 
-	const { lyrics } = props;
+  const { lyrics } = props;
 
-	const onLike = (id, likes) => {
-		likeLyric({
-			variables: { id },
-			optimisticResponse: {
-				__typename: "Mutation",
-				likeLyric: {
-					id: id,
-					__typename: "LyricType",
-					likes: likes + 1
-				}
-			}
-		});
-	};
+  const onLike = (id, likes) => {
+    likeLyric({
+      variables: { id },
+      optimisticResponse: {
+        __typename: "Mutation",
+        likeLyric: {
+          id: id,
+          __typename: "LyricType",
+          likes: likes + 1
+        }
+      }
+    });
+  };
 
-	const renderLyrics = () => {
-		return lyrics.map(lyric => {
-			return (
-				<li key={lyric.id} className="collection-item">
-					{lyric.content}
-					<div className="vote-box">
-						<i
-							className="material-icons"
-							onClick={() => onLike(lyric.id, lyric.likes)}
-						>
-							thumb_up
-						</i>
-						{lyric.likes}
-					</div>
-				</li>
-			);
-		});
-	};
-	return (
-		<div>
-			<ul className="collection">{renderLyrics()}</ul>
-		</div>
-	);
+  const renderLyrics = () => {
+    return lyrics.map(lyric => {
+      return (
+        <li key={lyric.id} className="collection-item">
+          {lyric.content}
+          <div className="vote-box">
+            <i
+              className="material-icons"
+              onClick={() => onLike(lyric.id, lyric.likes)}
+              style={likeButtonStyle}
+            >
+              thumb_up
+            </i>
+            {lyric.likes}
+          </div>
+        </li>
+      );
+    });
+  };
+  return (
+    <div>
+      <ul className="collection">{renderLyrics()}</ul>
+    </div>
+  );
+};
+
+const likeButtonStyle = {
+  color: "#f37e21"
 };
 
 export default LyricList;
