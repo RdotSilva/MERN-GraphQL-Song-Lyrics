c48b8dbc39405db32d8769cda95c49eb2c9f652a
RdotSilva <RyandotSilva@gmail.com>
2019-12-20 18:24:16 -0500

    Add NavBar to SongList component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Fragment } from "react";
+
 import { gql } from "apollo-boost";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 
@@ -7,43 +8,48 @@ import { Link } from "react-router-dom";
 import FETCH_SONGS from "../queries/fetchSongs";
 import DELETE_SONG from "../queries/deleteSong";
 
+import NavBar from "./layout/NavBar";
+
 const SongList = () => {
-	const { loading, error, data } = useQuery(FETCH_SONGS);
+  const { loading, error, data } = useQuery(FETCH_SONGS);
 
-	const [deleteSong] = useMutation(DELETE_SONG, {
-		refetchQueries: ["FetchSongs"]
-	});
+  const [deleteSong] = useMutation(DELETE_SONG, {
+    refetchQueries: ["FetchSongs"]
+  });
 
-	const onSongDelete = id => {
-		deleteSong({
-			variables: { id }
-		});
-	};
+  const onSongDelete = id => {
+    deleteSong({
+      variables: { id }
+    });
+  };
 
-	const renderSongs = () => {
-		return data.songs.map(song => {
-			return (
-				<li key={song.id} className="collection-item">
-					<Link to={`/songs/${song.id}`}>{song.title}</Link>
-					<i className="material-icons" onClick={() => onSongDelete(song.id)}>
-						delete
-					</i>
-				</li>
-			);
-		});
-	};
+  const renderSongs = () => {
+    return data.songs.map(song => {
+      return (
+        <li key={song.id} className="collection-item">
+          <Link to={`/songs/${song.id}`}>{song.title}</Link>
+          <i className="material-icons" onClick={() => onSongDelete(song.id)}>
+            delete
+          </i>
+        </li>
+      );
+    });
+  };
 
-	if (loading) return <p>Loading...</p>;
-	if (error) return <p>Error :(</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
 
-	return (
-		<div className="container">
-			<ul className="collection">{renderSongs()}</ul>
-			<Link to="/songs/new" className="btn-floating btn-large red right">
-				<i className="material-icons">add</i>
-			</Link>
-		</div>
-	);
+  return (
+    <Fragment>
+      <NavBar></NavBar>
+      <div className="container">
+        <ul className="collection">{renderSongs()}</ul>
+        <Link to="/songs/new" className="btn-floating btn-large red right">
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
+    </Fragment>
+  );
 };
 
 export default SongList;
