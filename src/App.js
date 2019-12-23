4e33dc9872ed7c60f2070270f06616013c2d73de
RdotSilva <RyandotSilva@gmail.com>
2019-12-22 20:25:32 -0500

    Add redirect to songlist page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import "./style/style.css";
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
 import ApolloClient, { InMemoryCache } from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
@@ -12,24 +17,27 @@ import SongDetail from "./components/SongDetail";
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-	uri: "http://localhost:4000/graphql",
-	cache
+  uri: "http://localhost:4000/graphql",
+  cache
 });
 
 class App extends Component {
-	render() {
-		return (
-			<ApolloProvider client={client}>
-				<Router>
-					<Switch>
-						<Route exact path="/songlist" component={SongList} />
-						<Route exact path="/songs/new" component={SongCreate} />
-						<Route exact path="/songs/:id" component={SongDetail} />
-					</Switch>
-				</Router>
-			</ApolloProvider>
-		);
-	}
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <Switch>
+            <Route exact path="/songlist" component={SongList} />
+            <Route exact path="/songs/new" component={SongCreate} />
+            <Route exact path="/songs/:id" component={SongDetail} />
+            <Route path="/">
+              <Redirect to="/songlist" />
+            </Route>
+          </Switch>
+        </Router>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
