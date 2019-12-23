25dae4ddc1ef21a0fa280ffa09d53cfb69b79c03
RdotSilva <RyandotSilva@gmail.com>
2019-12-23 14:59:07 -0500

    Fix typo

--- server/schema/root_query_type.js
+++ server/schema/root_query_type.js
@@ -6,29 +6,29 @@ const Lyric = require("../models/lyric");
 const Song = require("../models/song");
 
 const RootQuery = new GraphQLObjectType({
-	name: "RootQueryType",
-	fields: () => ({
-		songs: {
-			type: new GraphQLList(SongType),
-			resolve() {
-				return Song.find({});
-			}
-		},
-		song: {
-			type: SongType,
-			args: { id: { type: new GraphQLNonNull(GraphQLID) } },
-			resolve(parentValue, { id }) {
-				return Song.findById(id);
-			}
-		},
-		lyric: {
-			type: LyricType,
-			args: { id: { type: new GraphQLNonNull(GraphQLID) } },
-			resolve(parnetValue, { id }) {
-				return Lyric.findById(id);
-			}
-		}
-	})
+  name: "RootQueryType",
+  fields: () => ({
+    songs: {
+      type: new GraphQLList(SongType),
+      resolve() {
+        return Song.find({});
+      }
+    },
+    song: {
+      type: SongType,
+      args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+      resolve(parentValue, { id }) {
+        return Song.findById(id);
+      }
+    },
+    lyric: {
+      type: LyricType,
+      args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+      resolve(parentValue, { id }) {
+        return Lyric.findById(id);
+      }
+    }
+  })
 });
 
 module.exports = RootQuery;
