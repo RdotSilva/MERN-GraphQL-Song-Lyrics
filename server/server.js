f24e70eec626c430945d34f26404f3a35bf25f27
RdotSilva <RyandotSilva@gmail.com>
2019-12-25 21:42:18 -0500

    Update graphqlHTTP

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const expressGraphQL = require("express-graphql");
+const graphqlHTTP = require("express-graphql");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 
 app.use(
   "/graphql",
-  expressGraphQL({
+  graphqlHTTP({
     schema,
     graphiql: true
   })
