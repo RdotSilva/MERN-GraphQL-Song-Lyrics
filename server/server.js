3ce4094139dcdf2e10939976b721d5ffa9c8b5af
RdotSilva <RyandotSilva@gmail.com>
2019-12-25 21:38:42 -0500

    Add static options to server

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "../config/config.env" });
 
 const app = express();
-const port = process.env.PORT || 4000;
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
@@ -24,8 +23,6 @@ app.get("/", (req, res) => {
   res.send("Server is up!");
 });
 
-app.listen(port, () => console.log(`Server running on port: ${port}`));
-
 app.use(
   "/graphql",
   expressGraphQL({
@@ -33,5 +30,15 @@ app.use(
     graphiql: true
   })
 );
+
+app.use(express.static("build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
 
 module.exports = app;
