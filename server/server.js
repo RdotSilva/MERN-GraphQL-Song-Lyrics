278a632fc4a78b3d7cec7cf49f5698a8adb44195
RdotSilva <RyandotSilva@gmail.com>
2020-01-14 10:44:08 -0500

    Update server config

--- server/server.js
+++ server/server.js
@@ -19,25 +19,24 @@ mongoose
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Server is up!");
-});
+app.use(express.static("build"));
+
+// app.get("/", (req, res) => {
+//   res.send("Server is up!");
+// });
 
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema,
-    graphiql: true
+    schema
   })
 );
-
-app.use(express.static("build"));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
 
