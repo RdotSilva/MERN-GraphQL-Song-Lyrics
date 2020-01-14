448df64282adb6191b57d53d4f85d9f19ba870b4
RdotSilva <RyandotSilva@gmail.com>
2020-01-14 11:14:10 -0500

    Update server build location

--- server/server.js
+++ server/server.js
@@ -19,8 +19,6 @@ mongoose
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(express.static("build"));
-
 // app.get("/", (req, res) => {
 //   res.send("Server is up!");
 // });
@@ -32,11 +30,13 @@ app.use(
   })
 );
 
+app.use(express.static("build"));
+
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../build", "index.html"));
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
 
