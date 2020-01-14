223431b60a0d68b2b03fede4202aa958d1a2363a
RdotSilva <RyandotSilva@gmail.com>
2020-01-14 15:51:51 -0500

    Update build path

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.use(
 app.use(express.static("build"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../build", "index.html"));
 });
 
 const port = process.env.PORT || 5000;
